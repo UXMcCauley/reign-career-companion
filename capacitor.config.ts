41de1ee13961f31e896b8c577de2158e95a66fa2
DRUMAC <83826725+UXMcCauley@users.noreply.github.com>
2026-06-18 07:15:58 -0500

    Update capacitor.config.ts to include LiveUpdates plugin configuration and modify iOS project settings to disable user script sandboxing for improved app performance.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mccauley.reigncareercompanion',
   appName: 'photo-gallery',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LiveUpdates: {
+      appId: 'b778e185',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
