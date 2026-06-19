80421a475429eb630798eda7a05e1ad10e312a2f
DRUMAC <83826725+UXMcCauley@users.noreply.github.com>
2026-06-19 18:01:21 -0500

    Add iOS background color to capacitor.config.ts and clean up CSS background styles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.mccauley.reigncareercompanion',
   appName: 'REIGN Career Companion',
   webDir: 'dist',
+  ios: {
+    backgroundColor: '#070814'
+  },
   plugins: {
     LiveUpdates: {
       appId: '5d236a33',
